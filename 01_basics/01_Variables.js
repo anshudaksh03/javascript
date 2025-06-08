8fdc0c18ac03a5fc845409e141d752ac0b8c2e63
AnnShu <anshudakah03@gmail.com>
2025-06-08 01:34:49 +0000

    container config done

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,6 @@
+const accountId = 12344
+let AccountEmail = "anshu936876@gmail.com"
+var AccountPassword = "9378478478"
+accountCity = "Delhi"
+ //accountId = 5432e643 // not allowerd this repeat again and again in const because this is fixed not changeable
+ console.log(accountId)
