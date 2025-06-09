721f070060dfdbd620e0a3f92a7233e168d9dcd0
AnnShu <anshudakah03@gmail.com>
2025-06-09 00:10:10 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,28 @@
+"Use strict"; //Treat all js code as newer version
+
+// alert(3 + 3) // we are useing node js note browser
+
+console.log( 3 +
+    
+    3)   //We should always know about our code readbility 
+
+    console.log("Anshu")
+    let name = "Anshu" // String
+    let age = 21; // number /num
+let isLoggedin =  true; // boolean type
+
+//Premitive data type
+
+// number => 2 to power 53
+// BigInt when we use this we have big number then
+// String => "Anshu"\
+//boolean => "True/False"
+//null => Standlone value it means empty donot recevive any type of information and data
+//undefiend = when we not mention our value we decaleared variable but not mention value
+// Symbol => unique
+
+//Object
+
+console.log(typeof age) //32 number
+console.log(typeof null) // object
+console.log(typeof undefined) //undefiend
