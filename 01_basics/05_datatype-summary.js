13bcf22c370c21bb7f104dcb39a498e76855d093
AnnShu <anshudakah03@gmail.com>
2025-06-15 09:31:00 +0000

    datatype and comperisions

---
+++ 01_basics/05_datatype-summary.js
@@ -0,0 +1,20 @@
+//Primitive data type
+
+//7 type : String, number, boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 103.3
+const loggedIn = false
+const outsideTemp = null
+let userEmail ; //undefined
+const id = Symbol('234')
+const anotherId = Symbol('234')
+
+console.log(id === anotherId) // values is note same // out false
+
+const bigNumber =  2823456789009876543n
+
+//Refrence (Non - primitive data type)
+//Arrays , object , functions
+
+const heros = ["    "]
