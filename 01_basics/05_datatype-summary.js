dadb34f91c4e168d968691d7684c2cfe45415a92
AnnShu <anshudakah03@gmail.com>
2025-06-15 10:11:36 +0000

    typeof

--- 01_basics/05_datatype-summary.js
+++ 01_basics/05_datatype-summary.js
@@ -10,11 +10,25 @@ let userEmail ; //undefined
 const id = Symbol('234')
 const anotherId = Symbol('234')
 
-console.log(id === anotherId) // values is note same // out false
+//console.log(id === anotherId) // values is note same // out false
 
 const bigNumber =  2823456789009876543n
 
 //Refrence (Non - primitive data type)
 //Arrays , object , functions
 
-const heros = ["    "]
+const heros = ["Shaktiman", "nanggraj", " doga"] // Array
+
+let myobj =  {      //object
+    name: "Anshu",
+    age: 21
+}
+//console.log(myobj,heros)
+
+ const myfunction = function(){       //function
+console.log("hello world")
+}
+console.log(typeof myfunction)
+
+//list https://262.ecma-international.org/5.1/#sec-11.4.3
+
