82f87b275b13c1a8df317a331d1d4999dd3f7f96
AnnShu <anshudakah03@gmail.com>
2025-06-18 00:59:19 +0000

    array

---
+++ 01_basics/02_basics/01_array.js
@@ -0,0 +1,6 @@
+//Array  storing a collection of multiple items under a single variable name
+// () parenthesis {} brecisses [] bracket
+
+let myarr = [0,1,2,3,4,5,6,7,true,"Anshu"]
+console.log(myarr[0])//0
+
