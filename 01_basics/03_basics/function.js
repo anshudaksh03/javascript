9e63c92ca68ce2aa2b40af44fc85f040771e8ba6
AnnShu <anshudakah03@gmail.com>
2025-07-04 00:41:51 +0000

    function with object and array

--- 01_basics/03_basics/function.js
+++ 01_basics/03_basics/function.js
@@ -55,7 +55,7 @@ console.log(loginUserMessage())*/
 // console.log(loginUserMessage())
 
 
-function loginUserMessage (username = "Sam"){
+/*function loginUserMessage (username = "Sam"){
     if(!username){
         console.log("please enter username") 
         return
@@ -63,4 +63,36 @@ function loginUserMessage (username = "Sam"){
     return `${username} just logged in`
 }
 //console.log(loginUserMessage()) // agar valuse pass kro ga to sam ko overwrite kr diya jaye gas
-console.log(loginUserMessage("Anshu")) 
+console.log(loginUserMessage("Anshu")) */
+
+
+function calculatecardPrice (...num1) { //...rest operator
+    return num1
+}
+//console.log(calculatecardPrice(200,582,700))
+
+const user = {
+    username: "Anshu",
+    price:199
+}
+
+function handleobject (anyobject){
+    console.log(`user name is ${anyobject.username} and price is ${anyobject.price}`)
+
+}
+ // abb object in function
+ handleobject(user) //user name is Anshu and price is 199
+//  handleobject({
+//     username:"sam",
+//     price:199
+//  })  
+
+ // array in function
+
+ const newarray = [200, 300, 400 , 500]
+
+ function returnSecondvalue (getarray){
+    return getarray[0]
+ }
+ console.log(returnSecondvalue(newarray)) // out-user name is sam and price is 199
+300
