0dc475914a6a6b78fead6a926eceb209b3e11dba
AnnShu <anshudakah03@gmail.com>
2025-07-29 00:51:35 +0000

    prototype

---
+++ Javascriptfullcourse/10_oop_and_classes/objectprototype.js
@@ -0,0 +1,12 @@
+function multiplyBy5(num) {
+    return num * 5;
+}
+multiplyBy5.power = 2;
+console.log(multiplyBy5(5))
+console.log(multiplyBy5.power);
+console.log(multiplyBy5.prototype);
+
+//---------------------------------------------------------------// node Javascriptfullcourse/10_oop_and_classes/objectprototype.js
+
+
+
