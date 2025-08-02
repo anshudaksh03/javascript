473d94befa3e4046ee2c4e2be55ad0d17fcbd7f5
AnnShu <anshudakah03@gmail.com>
2025-08-02 00:45:25 +0000

    statics

--- Javascriptfullcourse/10_oop_and_classes/staticprop.js
+++ Javascriptfullcourse/10_oop_and_classes/staticprop.js
@@ -11,4 +11,17 @@ class user {
     }
 }
 const Anshu = new user("Anshu")
-console.log(Anshu.createId());
+//console.log(Anshu.createId());
+
+//---------------------same but check------------ 
+
+
+class teacher extends user{
+    constructor(username,email){
+        super(username)
+        this.email = email
+    }
+}
+const iphone = new teacher("iphone","i@phone.com")
+//iphone.logMe()
+console.log(iphone.createId()) // hum na static createid kr di to wo ab huma kise ka bhe access nahi lana da raha hai
