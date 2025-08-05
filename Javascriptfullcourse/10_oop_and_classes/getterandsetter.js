2f6c188cecd92db1fed06952819303a4af84b4ea
AnnShu <anshudakah03@gmail.com>
2025-08-05 01:00:00 +0000

    getterandsetters

---
+++ Javascriptfullcourse/10_oop_and_classes/getterandsetter.js
@@ -0,0 +1,11 @@
+class user{
+    constructor(email,password){
+this.email = email
+this.password - password
+    }
+}
+
+const Anshu = new user("anshu@gmail.com", "123")
+console.log(Anshu.email);
+
+//---------runer === node Javascriptfullcourse/10_oop_and_classes/getterandsetter.js
