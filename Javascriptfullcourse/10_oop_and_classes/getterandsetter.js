b23e59d872e6c25af9b837056a63475e43731183
AnnShu <anshudakah03@gmail.com>
2025-08-12 00:54:47 +0000

    getter setter

--- Javascriptfullcourse/10_oop_and_classes/getterandsetter.js
+++ Javascriptfullcourse/10_oop_and_classes/getterandsetter.js
@@ -1,11 +1,28 @@
 class user{
     constructor(email,password){
 this.email = email
-this.password - password
+this.password = password
     }
+//-----------------------email getter setter----------
+
+get email(){
+    return this._email.toUpperCase()
 }
 
-const Anshu = new user("anshu@gmail.com", "123")
+set email(value){
+  this._email = value
+}
+
+//--------------------Password-----------
+    // get password(){
+    //     return this._password.toUpperCase()
+    // }
+    // set password(value){
+    //     this._password = value.toUpperCase()
+    // }
+}
+
+const Anshu = new user("anshu@gmail.com", "123abcde")
 console.log(Anshu.email);
 
 //---------runer === node Javascriptfullcourse/10_oop_and_classes/getterandsetter.js
